a6dce067d555a57167d92f3fd8b5ac62f80d4354
roro123323 <130724642+roro123323@users.noreply.github.com>
2023-08-30 20:12:26 -0500

    second commit

--- EmployeeSQL/sql_table_schema.sql
+++ EmployeeSQL/sql_table_schema.sql
@@ -1,29 +1,29 @@
 
 CREATE TABLE `Departments` (
-	`dept_name` VARCHAR  NOT NULL ,
-    `dept_no` VARCHAR  NOT NULL PRIMARY KEY  ,
+	`dept_name` VARCHAR(50)   PRIMARY KEY ,
+    `dept_no` VARCHAR(50)  NOT NULL   ,
 );
 
 
 CREATE TABLE `Employees` (
-    `emp_no` INT  NOT NULL PRIMARY KEY ,
+    `emp_no` INT  PRIMARY KEY ,
     `birth_date` date  NOT NULL ,
-    `first_name` VARCHAR  NOT NULL ,
-    `last_name` VARCHAR  NOT NULL ,
+    `first_name` VARCHAR(50)  NOT NULL ,
+    `last_name` VARCHAR(50)  NOT NULL ,
 	`hire_date` date  NOT NULL ,
-    `sex` VARCHAR  NOT NULL ,
+    `sex` VARCHAR(50)  NOT NULL ,
     )
 	
 	CREATE TABLE `Department_Manager` (
-    `dept_no` VARCHAR  NOT NULL ,
-    `emp_no` int  NOT NULL ,
+    `dept_no` VARCHAR(50)  NOT NULL ,
+    `emp_no` INT  NOT NULL ,
     `past_date` date  NOT NULL ,
     `present_date` date  NOT NULL 
 );
 	
 	
 	CREATE TABLE `Department_Employees` (
-	`dept_no` VARCHAR  NOT NULL
+	`dept_no` VARCHAR(50)  NOT NULL
     `emp_no` INT  NOT NULL ,
     `past_date` date  NOT NULL ,
     `present_date` date  NOT NULL 
@@ -31,15 +31,15 @@ CREATE TABLE `Employees` (
 	
 	
 	CREATE TABLE `Salaries` (
-    `emp_no` int  NOT NULL ,
+    `emp_no` INT  NOT NULL ,
     `salary` INTEGER  NOT NULL ,
     `past_date` date  NOT NULL ,
     `present_date` date  NOT NULL 
 );
 
 CREATE TABLE `Titles` (
-    `emp_no` int  NOT NULL ,
-    `title` VARCHAR  NOT NULL ,
+    `emp_no` INT  NOT NULL ,
+    `title` VARCHAR(50)  NOT NULL ,
     `past_date` date  NOT NULL ,
     `present_date` date  NOT NULL 
 );
